35b076f036a02ed628fd71ada8930f5429caf1e3
nadiaxx <nadiasheikh4313@gmail.com>
2022-05-29 11:13:34 +0600

    Add server

---
+++ api/routes/students.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Get Request"
+    })
+})
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: "Post Request"
+    })
+})
+module.exports = router;
