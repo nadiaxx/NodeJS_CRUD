4e9d0f22ae8de19223bbfc314f0e0a4df0813852
nadiaxx <nadiasheikh4313@gmail.com>
2022-06-04 23:21:19 +0600

    add CRUD_operation

--- api/models/student.js
+++ api/models/student.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose'); 
+const mongoose = require('mongoose');
+ 
 //for designing schema we use builtinfunction of mongoose Schema and then declare which kind of coloums or rows and which type 
 //where ObjectID is a built in unique values and name,batch taken as string then exported it 
 
--- api/routes/students.js
+++ api/routes/students.js
@@ -56,5 +56,4 @@ router.delete('/:studentId',(req,res,next) => {
 
 })
 
-
 module.exports = router;
